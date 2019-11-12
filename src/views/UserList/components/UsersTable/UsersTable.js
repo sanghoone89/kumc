950ff74c8e04cf9c6247b15dde71b91e0d823d3f
sanghoone <sanghoone.jung@hotmail.com>
2019-11-12 14:14:08 +0900

    kumc user 추가

--- src/views/UserList/components/UsersTable/UsersTable.js
+++ src/views/UserList/components/UsersTable/UsersTable.js
@@ -3,11 +3,24 @@ import MUIDataTable from 'mui-datatables';
 import PropTypes from 'prop-types';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import theme from 'theme';
+import CustomTooltip from "components/custom/CustomTooltip";
 class UsersTable extends React.Component {
   getMuiTheme = () => theme;
+
+  rowClick = (rowData, rowMeta) => {
+    this.props.handleRowClickOpen(rowData);
+  };
+
   render() {
-    console.log('getMuiTheme :', this.getMuiTheme());
-    const columns = ['Name', 'Alias', 'NickName', 'Remain'];
+    const { handleClickOpen, handleMemberList } = this.props;
+
+    const columns = [
+      { name: "memberid", label: "번호" }, //options: { filter: false, sort: false, display: false } },
+      { name: "username", label: "이름" },
+      { name: "nickname", label: "닉네임" },
+      { name: "email", label: "이메일" },
+      { name: "position", label: "직책" }
+    ]
     const options = {
       filterType: 'dropdown',
       responsive: 'scrollMaxHeight',
@@ -15,7 +28,46 @@ class UsersTable extends React.Component {
       fiexdHeader: true,
       print: false,
       download: false,
-      elevation: 1
+      elevation: 1,
+      rowsPerPage: 30,
+      rowsPerPageOptions: [30, 60, 120],
+      onRowClick: this.rowClick,
+      customToolbar: () => {
+        return (
+          <CustomTooltip handleClickOpen={handleClickOpen} />
+        );
+      },
+      textLabels: {
+        toolbar: {
+          search: "검색",
+          viewColumns: "선택 검색",
+          filterTable: "필터 검색"
+        },
+        body: {
+          noMatch: "일치하는 데이터가 없습니다",
+          toolTip: "Sort",
+        },
+        pagination: {
+          next: "Next Page",
+          previous: "Previous Page",
+          rowsPerPage: "Rows per page:",
+          displayRows: "of",
+        },
+        filter: {
+          all: "All",
+          title: "FILTERS",
+          reset: "RESET",
+        },
+        viewColumns: {
+          title: "Show Columns",
+          titleAria: "Show/Hide Table Columns",
+        },
+        selectedRows: {
+          text: "row(s) selected",
+          delete: "Delete",
+          deleteAria: "Delete Selected Rows",
+        },
+      }
     };
     return (
       <MuiThemeProvider theme={this.getMuiTheme()}>
